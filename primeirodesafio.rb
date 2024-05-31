aceda9be6ffbadfe684cc66d78c57c99adb260a4
Bruno Lopes <40304257+brunoloopes@users.noreply.github.com>
2024-05-31 10:40:00 -0300

    Create primeirodesafio.rb

---
+++ primeirodesafio.rb
@@ -0,0 +1,12 @@
+#Você precisa criar um programa simples de acesso a leitores em uma biblioteca e para isso o usuário deverá digitar seu nome(string) e o sobrenome(string)
+#, além da sua idade que é um dado(int)
+#tudo isso precisa ser impresso, em uma única frase.
+
+print "Digite seu nome: "
+nome = gets.chomp
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+print "Digite sua idade: "
+idade = gets.chomp
+
+puts "Olá Seu nome e: #{nome.to_s} #{sobrenome.to_s} e sua idade e: #{idade.to_i}"
